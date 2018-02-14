6887b9bf213d312803ff0a9e94591268270dec41
antonpetersson <antonpetersson@users.noreply.github.com>
2018-02-14 14:22:37 +0100

    header/menu update

--- include/header.php
+++ include/header.php
@@ -14,13 +14,28 @@
         <h1>Valentines</h1>
         </div>
         <div id="menu">
-            <ul>
-                <li>Start</li>
-                <li>Valentines-tips</li>
-                <li>Om Valentines day</li>
-                <li>Singel-sidan</li>
-                <li>Kontakt</li>
-                <li>Om oss</li>
+            <ul id="menuUL">
+                <li><a href="">Start</a></li>
+                <li><a href=""></a></li>
+
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">Valentines-tips</a>
+                    <div class="dropdown-content">
+                        <a href="#">Present-tips</a>
+                        <a href="#">Film-tips</a>
+                        <a href="#">Restaurang-tips</a>
+                        <a href="#">Godis-tips</a>
+                        
+                    </div>
+                </li>
+
+
+
+
+                <li><a href="">Om Valentines day</a></li>
+                <li><a href="">Singel-sidan</a></li>
+                <li><a href="">Kontakt</a></li>
+                <li><a href="">Om oss</a></li>
         </div>
         <div id="favoritesDropdown">
         <p>favoriter</p>
