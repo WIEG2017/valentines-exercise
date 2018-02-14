735b9e7dd7c23deaf46b4483e81e9e0fc2b18527
Faroch Mehri <faroch.mehri@gmail.com>
2018-02-14 17:03:35 +0100

    Data && Images

--- incl/data.php
+++ incl/data.php
@@ -31,50 +31,58 @@
         1 => array(
             'catid'   => 1,
             'name'    => 'Oliver',
-            'image'   => './images/nalleb-e1512081740326.jpg',
+            'image'   => './images/nalle1.jpg',
             'dscrptn' => 'Jag är en lurvig och kramgo kompis på ca 10 cm och är Nallebudets populäraste.',
+            'price'   => 299
         ),
         2 => array(
             'catid'   => 1,
             'name'    => 'Beige Kanin',
-            'image'   => './images/nalle_kanin.jpg',
+            'image'   => './images/nalle2.jpg',
             'dscrptn' => 'Jag är den superpopulära fluffiga kaninen Bashful Bunny, och är sådär extra mjuk och härlig.',
+            'price'   => 249
         ),
         3 => array(
             'catid'   => 1,
             'name'    => 'VEM-Katten',
-            'image'   => './images/nalle_vem.jpg',
+            'image'   => './images/nalle3.jpg',
             'dscrptn' => 'Jag är en färgglad katt från de populära Vem?-böckerna av Stina Wirsén, jag är designad i samarbete med Anna Hörling.',
+            'price'   => 449
         ),
         4 => array(
             'catid'   => 1,
             'name'    => 'Nalle med rött hjärta',
-            'image'   => './images/7046.jpg',
+            'image'   => './images/nalle4.jpg',
             'dscrptn' => 'Lite större nalle ( ca 16 cm) som håller i ett stort rött hjärta',
+            'price'   => 399
         ),
         5 => array(
             'catid'   => 2,
             'name'    => 'Romantikboost',
-            'image'   => './images/BOU1746S.jpg',
-            'dscrptn' => 'Superromantisk bukett fylld med röda, rosa och cremefärgade rosor och säsongens gröna blad.'
+            'image'   => './images/blomma1.jpg',
+            'dscrptn' => 'Superromantisk bukett fylld med röda, rosa och cremefärgade rosor och säsongens gröna blad.',
+            'price'   => 349
         ),
         6 => array(
             'catid'   => 2,
             'name'    => 'Kärleksfull',
-            'image'   => './images/BOU17_136S.jpg',
+            'image'   => './images/blomma2.jpg',
             'dscrptn' => 'Röda rosor med ett fång underbara rosa vaxblommor. Vas ingår ej men kan köpas som en tillvalsprodukt.',
+            'price'   => 299
         ),
         7 => array(
             'catid'   => 2,
             'name'    => 'Rosor och gerbera',
-            'image'   => './images/BOU17_21S.jpg',
+            'image'   => './images/blomma3.jpg',
             'dscrptn' => 'En vacker bukett i rött och rosa. Tips! Du kan köpa till smarriga chokladpraliner i nästa steg.',
+            'price'   => 199
         ),
         8 => array(
             'catid'   => 2,
             'name'    => 'Passion',
-            'image'   => './images/BOU10_149S.jpg',
+            'image'   => './images/blomma4.jpg',
             'dscrptn' => 'Ett fantastiskt exempel på en traditionell, röd rosbukett med brudslöja, frodig grönska och dekorativa hjärtan.',
+            'price'   => 249
         ),
     );
 
