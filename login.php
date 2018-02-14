8f31fba4f0cba51296df3fd939d8fa21b9563496
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-02-14 17:54:07 +0100

    update login.php
    inlogg fungerar

--- login.php
+++ login.php
@@ -1,18 +1,40 @@
 <?php
+    session_start();//ska flyttas in i headern 
     include "incl/header.php";
-?>
-<wrapper>
-<form>
-<input type="text" placeholder=" Email" name="email"/>
-<input type="text" placeholder=" Password" name="password"/>
-<button class="loginButton" type="submit">Logga in</button>
-</form> 
-</wrapper>
+    require "incl/data.php"; //ska egentligen va inlagt i headern 
+   
+   
+   if(isset($_SESSION['CID'])) header('Location: ./index.php');
+   
+   $error = false;
+   if(isset($_POST['email']) && isset($_POST['password'])) {
+       $_POST['email']    = strtolower($_POST['email']);
+       $_POST['password'] = strtolower($_POST['password']);
 
+       foreach($customers as $key => $value) {
+           if(($value['email'] == $_POST['email']) && ($value['password'] == $_POST['password'])) {
+               $_SESSION['CID'] = $key;
+               header('Location: ./index.php');
+           }
+       }
+       
+       $error = true;
+   }
+?>
+
+<?php
+   if($error) echo '<h2 style="color: red;">Användarnamn eller lösenord är fel!</h2>';
+?>
+<form method="POST">
+   <input type="text" placeholder=" Email" name="email"/>
+   <input type="text" placeholder=" Password" name="password"/>
+   <button class="loginButton" type="submit">Logga in</button>
+</form>
 
 <!-- Här ska vi ha ren HTML kod! -->
 
 <?php
-    include "incl/footer.php";
+   include "incl/footer.php";
 ?>
+
 
