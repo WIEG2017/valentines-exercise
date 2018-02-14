f45a0d6705e8f23d8e396f7df4e50ff3e2b5d32a
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-02-14 14:53:18 +0100

    update login.pgp, signup.pgp och css

--- signup.php
+++ signup.php
@@ -4,6 +4,14 @@
 
 <!-- Här ska vi ha ren HTML kod! -->
 
+<wrapper>
+<form>
+<input type="text" placeholder=" Email" name="email"/>
+<input type="text" placeholder=" Password" name="password"/>
+<button class="signupButton" type="submit">Bli medlem</button>
+</form> 
+</wrapper>
+
 <?php
     include "incl/footer.php";
 ?>
