779cde3aa3d1c85ba491565ad04df4142485cb94
amandaaab <amandaaab@users.noreply.github.com>
2018-02-14 15:15:44 +0100

    header struktur samt css fil

---
+++ header.php
@@ -0,0 +1,34 @@
+<?php $title = 'Valentine'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title><?php $title ?></title>
+</head>
+<body>
+    <div class="header">
+        <div class="valentine">V A L E N T I N E</div>
+            <div class="cart_menu">
+                <div class="menu">
+                    <a href="index.php" class="link">Start</a>
+                    <a href="#"class="link">Array</a>
+                    <a href="#"class="link">Pension</a>
+                    <a href="#"class="link">Blommor</a>
+                    <a href="#"class="link">Choklad</a>
+                    <a href="#"class="link">Kontakt</a>
+                    <a href="#"class="link">Om oss</a>
+                </div>
+                <div class="cart">Kundvagn</div>
+                <div class="fav_form">
+                    <div class="fav_item">
+                        Start <input type="checkbox">
+                    </div>
+                </div>
+        </div>
+    </div>
+    
+</body>
+</html>
