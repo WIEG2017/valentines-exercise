79212f812327ed842df54848fc569bbc0f738bbe
Thomas <ThommyDude@users.noreply.github.com>
2018-02-14 14:35:09 +0100

    Create contact.php
    It has a map and everything! :smile:

---
+++ contact.php
@@ -0,0 +1,45 @@
+<?php
+    include "incl/header.php";
+?>
+
+<div class="well well-sm">
+    <h3>
+        <strong>Visit us</strong>
+    </h3>
+</div>
+<div class="row">
+    <div class="col-md-7 map-holder">
+        <iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ2WUtn33zT0YR2uKjp362ry8&key=AIzaSyAlEpssURhMe60k5kkrTzRsf5ROXtZfQqc" allowfullscreen></iframe>
+    </div>
+    <div class="col-md-5">
+        <h4>
+            <strong>Leave us a message</strong>
+        </h4>
+        <form id="message">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Name">
+            </div>
+            <div class="form-group">
+                <input type="email" class="form-control" placeholder="E-mail">
+            </div>
+            <div class="form-group">
+                <input type="tel" class="form-control" placeholder="Phone number">
+            </div>
+            <div class="form-group">
+                <textarea class="form-control" rows="3" placeholder="Write your message here!">
+                </textarea>
+            </div>
+            <button class="btn btn-default" type="submit" name="button"><i class="fa fa-paper-plane-o" aria-hidden="true"></i> Send</button>
+        </form>
+    </div>
+</div>
+
+<p>
+Tri-tip short ribs t-bone biltong brisket swine alcatra. Short ribs corned beef chicken, turducken jerky swine fatback jowl cow sausage kielbasa tongue. Short loin porchetta salami, tri-tip prosciutto pastrami pig rump bacon sausage pork chop buffalo. Ground round bresaola alcatra, pork belly doner pig sirloin kevin rump boudin strip steak leberkas tongue.
+Pork salami ribeye, meatball frankfurter shankle pork chop jowl rump pork loin tail hamburger short ribs biltong brisket. Picanha pastrami chicken turducken jowl short loin short ribs shoulder t-bone. Turducken cupim drumstick jowl kielbasa, capicola bacon hamburger cow kevin meatloaf biltong. Picanha alcatra flank pork kevin shank andouille sirloin turkey tail ribeye venison ham hock pastrami pork chop. Capicola landjaeger swine sirloin meatloaf andouille ham hock cupim buffalo picanha alcatra pig kielbasa. Leberkas pork andouille cupim filet mignon pancetta kielbasa buffalo short ribs turducken. Tri-tip shankle shank meatloaf cupim, bacon short loin.
+Meatball landjaeger shankle, pastrami turkey prosciutto ham hock corned beef brisket. Shank bresaola salami pork chop rump tongue. Cupim ball tip beef filet mignon pork chop tri-tip. Alcatra pork chop meatloaf tri-tip. Kevin ham hock t-bone chuck short ribs brisket ham meatloaf, doner fatback ribeye shankle chicken capicola. Pork frankfurter meatball ground round, filet mignon rump short loin alcatra meatloaf.
+</p>
+
+<?php
+    include "incl/footer.php";
+?>
