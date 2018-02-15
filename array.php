9c096d3af1b7feec0bd8740ac286e0d6e0fc59db
Andreeez <32762098+Andreeez@users.noreply.github.com>
2018-02-15 14:16:32 +0100

    Array uppdatering

--- array.php
+++ array.php
@@ -4,17 +4,17 @@
     include 'include/loginfunction.php';
     include 'include/data.php';
     if($_SESSION['email']){
-      echo "du är inloggad";
+      foreach($listOfEachFlower as $key=>$array){
+        echo "<div>";
+        echo "<h1>$array[name]</h1>";
+        echo "<img src=".$array["image"].">";
+        echo "<p>$array[description]</p>";
+        echo "<h2>$array[price] kr</h2>";
+        echo "</div>";
+      }
     }else{
       include 'include/login.php'; 
     }
 
-    foreach($listOfEachFlower as $key=>$array){
-      echo "<div>";
-      echo "<h1>$array[name]</h1>";
-      echo "<img src=".$array["image"].">";
-      echo "<p>$array[description]</p>";
-      echo "<h2>$array[price] kr</h2>";
-      echo "</div>";
-    }
+
 ?>
