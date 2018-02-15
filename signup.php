6122e7d7b6657211413ef84b2e5e60737b1ec31c
Jonatan Josefsson <jonatanjosef@Jonatans-MacBook-Pro.local>
2018-02-15 14:27:21 +0100

    updated registration

--- signup.php
+++ signup.php
@@ -1,41 +1,31 @@
 <?php
-    session_start();
+    $page_title = "Sign Up";
+    if(count ($_POST) > 0) {
 
-        if(count ($_POST) > 0) {
-
-           $list = array(
-                'email'    => $_POST['email'],
-                'name'     => $_POST['username'],
-                'password' => $_POST['password'],  
-                
-            );
-        }
+        $list = array(
+             'email'    => $_POST['email'],
+             'name'     => $_POST['username'],
+             'password' => $_POST['password'],  
+             
+         );
+     }
+    include "incl/header.php";
+    
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="./css/style.css" />
-</head>
-<body>
-<div class="header">
-<h1>Register</h1>
-</div>
+<!-- Här ska vi ha ren HTML kod! -->
 
 <wrapper>
     <form method="post" action="register.php" class='formSignup'>
 
         <input type="text" class="textInput" placeholder=" username" name="username"/>
-        <input type="email" class="textInput" placeholder=" Email" name="email"/>
-        <input type="password" placeholder=" Password" name="password"/>
-        <button type="submit" name="register_btn" class="signupButton" value="">Bli medlem</button>
+        <input type="text" placeholder=" Email" name="email"/>
+        <input type="text" placeholder=" Password" name="password"/>
+        <button class="signupButton" type="submit">Bli medlem</button>
 
     </form> 
 </wrapper>
-    
-</body>
-</html> 
+
+<?php
+    include "incl/footer.php";
+?>
