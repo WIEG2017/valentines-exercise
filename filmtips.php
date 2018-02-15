714747980a463cfe195ae889e63735ed2c8c4042
andremedie <32762205+andremedie@users.noreply.github.com>
2018-02-15 13:17:57 +0100

    Filmtips first step
    Påbörjar bildmaterial och lägger till filmtips.php i projektet

---
+++ filmtips.php
@@ -0,0 +1,13 @@
+<?php 
+echo "<h1>Filmtips för Alla hjärtans dag 😻</h1>";
+
+echo "<p>1. ROMEO + JULIET</p><br/>";
+
+echo "<p>2. THE NOTEBOOK </p><br/>";
+
+echo "<p>3. BROKEBACK MOUNTAIN </p><br/>";
+
+echo "<p>3. TITANIC </p><br/>";
+
+
+?>
