4636e881a5f8f53b752558a6c4f2ffc8a16de7d6
Louise1987 <31926492+Louise1987@users.noreply.github.com>
2018-02-15 12:27:50 +0100

    testar

--- gifts.php
+++ gifts.php
@@ -9,40 +9,48 @@
 </head>
 <body>
 
+<!-- <a href="https://www.euroflorist.se/alla-hjartans-dag-ct1693?gclid=EAIaIQobChMI_7y52L6l2QIVBswYCh0ONwbBEAAYASAAEgKlgfD_BwE&gclsrc=aw.ds">select</a>
+<a href="https://www.nalleriet.se/alla-hjartans-dag-nallar/">select</a>
+<a href="http://www.inovadesign.se/hogtider/alla-hjartans-dag/">select</a> -->
+
 <?php
 
 $list = array ("blommor" => "200kr","mjukisdjur" => "150kr","smycken" => "700kr");
 print_r(array_values($list));
 
-foreach ($list as $site_title => $url){
-echo "<a href=$url>$site_title</a>";
-}
+
 
 ?>
+
 
 <?php
-function build_table($list){
-    $html = '<tabble>';  //start table
 
-    $html .='<tr>';
-    foreach ($list as $site_title => $url){
-        $html .= '<th>' . htmlproduct($key) . '</th>';
-    }
-    $html .= '</tr>';
+foreach ($list as $site_title => $url){
+    // echo "<a href=$url>$site_title</a><br>";
+    echo "<a href=https://www.euroflorist.se/alla-hjartans-dag-ct1693?gclid=EAIaIQobChMI_7y52L6l2QIVBswYCh0ONwbBEAAYASAAEgKlgfD_BwE&gclsrc=aw.ds></a>"
 
-    foreach ($list as $site_title => $url){
-        $html .= '<tr>';
-        
-    }
+
+function productGrid() {
+    echo "<img src='img/rosor.jpg' alt='bild kan inte visas' /><table><tr><th>'Blommor'</th>[0]</th><th><a href=https://www.euroflorist.se/alla-hjartans-dag-ct1693?gclid=EAIaIQobChMI_7y52L6l2QIVBswYCh0ONwbBEAAYASAAEgKlgfD_BwE&gclsrc=aw.ds></a></th></tr>'";
+    echo "<img src='img/nalle.jpg' alt='bild kan inte visas' /><table><tr><th>'Mjukisdjur'</th>[1]</th><th><a href=https://www.nalleriet.se/alla-hjartans-dag-nallar/></a></th></tr>'";
+    echo "<img src='img/smycken.jpg' alt='bild kan inte visas' /><table><tr><th>'Smycken'</th>[2]</th><th><a href=http://www.inovadesign.se/hogtider/alla-hjartans-dag/></a></th></tr>'";
+
+
+
+} 
 }
+
+
 
 
 ?>
 
 
-    <a href="https://www.euroflorist.se/alla-hjartans-dag-ct1693?gclid=EAIaIQobChMI_7y52L6l2QIVBswYCh0ONwbBEAAYASAAEgKlgfD_BwE&gclsrc=aw.ds">select</a>
-    <a href="https://www.nalleriet.se/alla-hjartans-dag-nallar/">select</a>
-    <a href="http://www.inovadesign.se/hogtider/alla-hjartans-dag/">select</a>
+
+
+    
+    
+    
 
 
 
