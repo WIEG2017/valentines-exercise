e5f7e71ae6b017a5cc838484bdd46821f7653d52
antonpetersson <antonpetersson@users.noreply.github.com>
2018-02-15 15:02:09 +0100

    fixat länkar

--- pages/filmtips.php
+++ pages/filmtips.php
@@ -1,24 +1,24 @@
 <?php
-include './include/header.php';
-include './include/functions.php';
+include '../include/header.php';
+include '../include/functions.php';
 ?>
 
 <?php 
 echo "<h1>Filmtips för Alla hjärtans dag 😻</h1>";
 
-echo "<img src='img/romeojuliet.jpg'</img>";
+echo "<img src='../img/romeojuliet.jpg'</img>";
 
 echo "<h1> ROMEO + JULIET</h1><br/>";
 
-echo "<img src='img/notebook.jpg'</img>";
+echo "<img src='../img/notebook.jpg'</img>";
 
 echo "<h1> THE NOTEBOOK </h1><br/>";
 
-echo "<img src='img/brokeback.jpg'</img>";
+echo "<img src='../img/brokeback.jpg'</img>";
 
 echo "<h1> BROKEBACK MOUNTAIN </h1><br/>";
 
-echo "<img src='img/titanic.png'</img>";
+echo "<img src='../img/titanic.png'</img>";
 
 echo "<h1> TITANIC </h1><br/>";
 
@@ -27,5 +27,5 @@ echo "<h1> TITANIC </h1><br/>";
 
 
 <?php
-    include './include/footer.php';
+    include '../include/footer.php';
 ?>
