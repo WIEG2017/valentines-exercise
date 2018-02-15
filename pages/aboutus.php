82bf52a3e1863fa03bf9e3c5ba30ac678c2c5c20
andremedie <32762205+andremedie@users.noreply.github.com>
2018-02-15 16:29:16 +0100

    Om oss sidan
    aboutus.php sidan - 'en liten berättelse om hur allt gick till ifrån början'. Work in progress!

---
+++ pages/aboutus.php
@@ -0,0 +1,23 @@
+<?php
+    include '../include/header.php';
+    include '../include/functions.php';
+?>
+
+<?php
+
+echo "<h4>Om Oss</h4>";
+
+echo "<h1>Berättelsen om VallyBally 📗📕</h1>";
+
+echo "<p>Året är 2018, fyra polare satt på plugget i Göteborg och brainstormar över just alla hjärtans dag.</p><br/>";
+
+echo "<p>Lång histora kort, vi fyra polare som består utav Martin, Louise, Anton och André fullföljde denna fenomenala idé och är idag, år 2026, en utav sveriges absolut största Alla hjärtans dag företag!</p>";
+
+echo "<p>De har som affärsidé att maximera människors upplevelser den 14e Februari, alltså alla hjärtans dag, detta har visat sig bli en dundersuccé. Från startskottet omsatte vi inte mer än 50 000kr med 4 anställda, idag 7 år senare omsätter vi 9,5 miljoner... euro, med 37 anställda.</p>";
+
+echo "<p>Vi på VallyBally hoppas och önskar er alla en trevlig Alla Hjärtans Dag!</p>";
+?>
+
+<?php
+    include '../include/footer.php';
+?>
