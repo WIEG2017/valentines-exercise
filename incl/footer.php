78ebe807dbe5d46c6d11bc091c80b6a7fee4d147
uhhi2000 <32762138+uhhi2000@users.noreply.github.com>
2018-02-15 15:50:28 +0100

    commit

--- incl/footer.php
+++ incl/footer.php
@@ -1,20 +1,20 @@
 <html>   
-        <footer>
-            <?php
-                if(isset($_SESSION['fav']))
+    <footer>
+        <?php
+            if(isset($_SESSION['fav']))
+            {
+                foreach($_SESSION['fav'] as $fav)
                 {
-                    foreach($_SESSION['fav'] as $fav)
-                    {
-                        $fav->drawMe();
-                    }
+                    $fav->drawMe();
                 }
-            ?>
+            }
+        ?>
 
-                <p>Copyright WIEG17</p>
-                <p>Contact information: Medieinstitutet</p>
-                <img src="images/logo.png" alt="ValentinesSchool" style="width: 5%; height: 5%;">
-                <p>valentines@example.com</p>
-        
-             </footer>
+        <p>Copyright WIEG17</p>
+        <p>Contact information: Medieinstitutet</p>
+        <img src="images/logo.png" alt="ValentinesSchool" style="width: 5%; height: 5%;">
+        <p>valentines@example.com</p>
+
+    </footer>
 
 </html>
