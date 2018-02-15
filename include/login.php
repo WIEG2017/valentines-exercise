4ba62ffeceb157dd63599fbedd8bbdf5122f8848
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2018-02-15 10:14:16 +0100

    login funktion för kassa.php och array.php

--- include/login.php
+++ include/login.php
@@ -1,12 +1,12 @@
 <?php
-include 'include/loginfunction.php';
-echo "<h1>Inlognings sidan</h1>";
+echo "<h1>Inlognings sida</h1>";
 echo "<form action='".$_SERVER["PHP_SELF"]."' method='post'>";
 echo "Email : ";
 echo "<input type='email' name='email'><br/><br/>";
 echo "<input type='submit' value='Login'>";
 echo "</form>";
-echo "<form action='".$_SERVER["PHP_SELF"]."' method='post'>";
+echo "<br/>";
+echo "<form  action='".$_SERVER["PHP_SELF"]."' method='post'>";
 echo "<input type='hidden' name='delete' value='true' >";
 echo "<input type='submit' value='Logout'>";
 echo "</form>";
