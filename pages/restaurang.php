26c57c71e70bd1b508062e274a6172cb6d7403d5
Louise1987 <31926492+Louise1987@users.noreply.github.com>
2018-02-16 11:46:34 +0100

    restaurang bildlänkar

--- pages/restaurang.php
+++ pages/restaurang.php
@@ -14,24 +14,19 @@
 </head>
 <body>
 
-<!-- <a href="http://ottomania.se/?utm_source=thatsup&utm_medium=referral"></a>
+<a href="http://ottomania.se/?utm_source=thatsup&utm_medium=referral"></a>
 <a href="http://restaurangmeet.se/wordpress/home/"></a>
 <a href="http://www.rumi.se/?utm_source=thatsup&utm_medium=referral"></a>
-<a href="http://www.dorsia.se/?utm_source=thatsup&utm_medium=referral"></a> -->
+<a href="http://www.dorsia.se/?utm_source=thatsup&utm_medium=referral"></a>
     
 <?php 
-// echo '<a href="http://ottomania.se/?utm_source=thatsup&utm_medium=referral"><img src="ottomania-3.jpg" /></a>';
-// echo '<a href="http://restaurangmeet.se/wordpress/home/"><img src="restaurang-meet-3.jpg" /></a>';
-// echo '<a href="http://www.rumi.se/?utm_source=thatsup&utm_medium=referral"><img src="rumi-persian-cuisine-1.jpg" /></a>';
-// echo '<a href="http://www.dorsia.se/?utm_source=thatsup&utm_medium=referral"><img src="Dorsia_01-LARGE.jpg" /></a>';
+echo '<a href="http://ottomania.se/?utm_source=thatsup&utm_medium=referral"><img src="/img/ottomania-3.jpg" /></a>';
+echo '<a href="http://restaurangmeet.se/wordpress/home/"><img src="/img/restaurang-meet-3.jpg" /></a>';
+echo '<a href="http://www.rumi.se/?utm_source=thatsup&utm_medium=referral"><img src="/img/rumi-persian-cuisine-1.jpg" /></a>';
+echo '<a href="http://www.dorsia.se/?utm_source=thatsup&utm_medium=referral"><img src="/img/Dorsia_01-LARGE.jpg" /></a>';
 ?>
 
-<?php
-$image_name = '/img/ottomania-3.jpg';
-$image_url = 'http://ottomania.se/?utm_source=thatsup&utm_medium=referral';
-?>
 
-<a href='<?php echo $image_url; ?>' border='0'><img src'<?php echo $image_name; ?>'></a>
 
 
 
