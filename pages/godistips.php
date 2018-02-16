30f0146ef4bb3b519d9ca07bee6159903d7f57c0
andremedie <32762205+andremedie@users.noreply.github.com>
2018-02-16 15:27:19 +0100

    godisTips updated!
    Godis-sidan med 8 olika tips för våra kunder!

--- pages/godistips.php
+++ pages/godistips.php
@@ -3,7 +3,41 @@ include '../include/header.php';
 include '../include/functions.php';
 ?>
 
+<?php
 
+echo "<h1>Godiva Drömchokladshjärtat</h1>";
+echo "<img src='/img/candy1.jpg'</img>";
+echo "<div>En stor låda med 76 olika chokladpraliner, ge din käraste en mumsig present. 1099;-</div>";
+
+echo "<h1>Lindt Lindor</h1>";
+echo "<img src='/img/candy2.jpg'</img>";
+echo "<div> - Chokladfyllda bollar med drömmig Nougat. Ju färre desto bättre, ät med din käraste och njut till fullo! 499;-</div>";
+
+echo "<h1>Merci Choklad</h1>";
+echo "<img src='/img/candy3.jpg'</img>";
+echo "<div>Kaffechoklad, hasselnötschoklad och 20 andra sorter till dig och din partner - 399;-</div>";
+
+echo "<h1>MjölkchokladsHjärtan</h1>";
+echo "<img src='/img/candy4.jpg'</img>";
+echo "<div>En klassiker ifrån år 1918, gammalt recept med fantastisk smaksensation. Njut! - 699:-</div>";
+
+echo "<h1>HallonFiskar</h1>";
+echo "<img src='/img/candy5.jpg'</img>";
+echo "<div>Drömgodiset som vi på VallyBally tycker är den bästa som finns - 199:- </div>";
+
+echo "<h1>Trolli - Sura Hjärtan</h1>";
+echo "<img src='/img/candy6.jpg'</img>";
+echo "<div>Sockerkick deluxe och många olika smaker i den sura genren. 89:- </div>";
+
+echo "<h1>Godishjärtan med Fina Ordvistelser</h1>";
+echo "<img src='/img/candy7.jpg'</img>";
+echo "<div>Detta är en utav de vackraste godisarna som vi säljer på VallyBally. Ni kommer förstå varför. - 4999:-</div>";
+
+echo "<h1>Alla Hjärtans Dag-Nörd-Godis</h1>";
+echo "<img src='/img/candy8.jpg'</img>";
+echo "<div>Denna är helt klart bäst, smakar bättre än de andra godisarna. - 149:- </div>";
+
+?>
 
 
 
