e9b6d0df850ccfa5c024b32670ac3276f7346543
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-02-16 14:03:42 +0100

    update index
    knapp bortagen

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 <div class="hero-text">
     <h1 style="text-shadow: 0 0 3px #FF0000;">Valentines Day Shop</h1>
     <p style="text-shadow: 0 0 3px #FF0000;">Köp presenter till din älskling</p>
-    <button>Tryck här typ</button>
     <br><br><br><br><br>
 </div>
 
