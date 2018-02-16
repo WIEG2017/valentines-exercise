20228fd18f2da260394d1265ec3103f08ce6fbc1
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2018-02-16 13:48:23 +0100

    kontakt klart

--- kontakt.php
+++ kontakt.php
@@ -2,6 +2,16 @@
     <div class="kontakt">  
     <h1>Kontakta oss</h1>
     <img class="kontaktImg" src='./bilder/kontaktaoss.png' >
+    <p>Öppettider</p>
+    <p>Måndag – Fredag: 09.00-18.00</p>
+    <p>Lördag: 09.00-13.00</p>
+    <p>Söndagar och helgdagar: Stängt</p>
+    <p>Euroflorist Europe B.</p>
+    <p>Euroflorists huvudkontor ligger i Holland.</p>
+    <p>Love st 250</p>
+    <p>1101 N HWY A14</p>
+    <p>The Netherlands</p>
+    <p>CVR-nr: NL802522129B01</p>
     </div>
 <?php 
 include 'include/footer.php';
