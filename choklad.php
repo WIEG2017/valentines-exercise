ae521b1bacae4378c68026e2c84883012bd570ef
Andreeez <32762098+Andreeez@users.noreply.github.com>
2018-02-16 10:17:25 +0100

    test i choklad.php

--- choklad.php
+++ choklad.php
@@ -3,6 +3,7 @@
     include 'include/header.php';
     include 'include/loginfunction.php';
     include 'include/data.php';
+ 
     if($_SESSION['email']){
       foreach($listofEachChoclate as $key=>$array){
         echo "<div>";
@@ -10,12 +11,19 @@
         echo "<img src=".$array["image"].">";
         echo "<p>$array[description]</p>";
         echo "<h2>$array[price] kr</h2>";
-        echo "<button>Lägg i varukorg</button>";
+        echo '<button onclick=\'showProduct()\'>Lägg i varukorg</button>';
         echo "</div>";
       }
+
+    function showProduct(){
+        echo 'alert("hej")';
+    }
+    
     }else{
       include 'include/login.php'; 
     }
 
+    
+   
 
 ?>
