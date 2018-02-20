a1f5a65bcf83304622600f70f9fa75702e3aa22b
pedramamiri <32764741+pedramamiri@users.noreply.github.com>
2018-02-20 12:08:43 +0100

    små css

--- include/login.php
+++ include/login.php
@@ -1,7 +1,7 @@
 <?php
 echo "<h1>Inlognings sida</h1>";
 echo "<form style='margin-left:10px;' action='".$_SERVER["PHP_SELF"]."' method='post'>";
-echo "Email : ";
+echo "<p style='color: white;font-size: 20px;'>Email : </p>";
 echo "<input style='width:250px;padding: 5px 5px;' type='email' name='email'><br/><br/>";
 echo "<input style='width:100px;padding: 5px 5px;' type='submit' value='Login'>";
 echo "</form>";
